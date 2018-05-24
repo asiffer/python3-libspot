bd4d99bad14e38234bbf1ad6b456d30e9b784016
asr <alban.siffer@irisa.fr>
2018-05-24 17:13:41 +0200

    add example

--- example/example.py
+++ example/example.py
@@ -1,29 +1,29 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-@author: asr
-"""
-
+import pyspot as ps # name of the python3 package
 import pandas as pd
-import pyspot as ps
 import matplotlib.pyplot as plt
 
+# loading data
 MAWI_17 = pd.read_csv("mawi_170812_50_50.csv", index_col=[0])
 MAWI_18 = pd.read_csv("mawi_180812_50_50.csv", index_col=[0])
 
+# we keep only the ratio of SYN packets
 rSYN_17 = MAWI_17['rSYN']
 rSYN_18 = MAWI_18['rSYN']
 
+# initialization of the Spot object
 q = 5e-4
 n_init = 2000
-
 spot = ps.Spot(q, n_init, up=True, down=False)
 
-# calibration
+
+## CALIBRATION
 for r in rSYN_17[-n_init:]:
 	spot.step(r)
 
-# anomaly detection
+
+## ANOMALY DETECTION
+
+# we init some structure to log what flags the algorithm
 up_threshold = [0.0] * len(rSYN_18)
 anomalies_x = []
 anomalies_y = []
@@ -44,8 +44,8 @@ f1, = plt.plot(up_threshold, ls='dashed', color="#AE81FF", lw=2)
 f2 = plt.scatter(anomalies_x, anomalies_y, color="#F55449")
 plt.legend([f1, f2], ['Threshold', 'Anomalies'])
 
-# we change the ticks to make it corresponds to the true time : 1 it = 50 ms
-plt.xticks(range(0,18001,3000), range(0,901,150))
+
+plt.xticks(range(0,18001,3000), range(0,901,150)) # we change the ticks to make it corresponds to the true time : 1 it = 50 ms
 plt.xlabel('Time (s)')
 plt.ylabel('Ratio of SYN packets')
-	
+
