eddcb8fc53fe0b988da211c10bbd2587bcb329d5
asr <alban.siffer@irisa.fr>
2018-09-11 14:57:56 +0200

    add setup.py for pip install

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 11 14:39:43 2018
+
+@author: asr
+"""
+
+from setuptools import setup
+
+setup(
+      name = "pyspot",
+      version = "1.1",
+      author = "Alban Siffer",
+      author_email = "alban.siffer@irisa.fr",
+      description = "Python3 wrapper to libspot",
+      url = "https://asiffer.github.io/libspot/python/",
+      py_modules=['pyspot']
+      )
+
